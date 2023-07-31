658eddd85a772cdc5a4eca0b1863088dadc43020
Aiden <skrillfalcoblast@icloud.com>
2023-07-31 13:50:54 +0300

    Adding csrf_trusted_origins.

--- Lyceum/Lyceum/settings.py
+++ Lyceum/Lyceum/settings.py
@@ -37,6 +37,8 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
+CSRF_TRUSTED_ORIGINS = ['https://*.bantopia.com']
+
 
 # Application definition
 
