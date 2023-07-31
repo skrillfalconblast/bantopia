5966be385a867a00344ba613d6d7aad9efbe5e4b
Aiden <skrillfalcoblast@icloud.com>
2023-07-31 09:52:38 +0300

    adding os environ

--- Lyceum/Lyceum/settings.py
+++ Lyceum/Lyceum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import environ
 
@@ -24,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.bantopia.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
