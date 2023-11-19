2a6d778bd1a6c1a092fc486b8212c0596fbc3efe
Aiden <skrillfalcoblast@icloud.com>
2023-11-19 22:07:41 +0000

    Removing the dashboard

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('create-profile/', views.createProfile, name='create_profile'),
-    path('<str:display_name>/dashboard/', views.dashboard, name='dashboard'),
-    path('<str:display_name>/edit-profile', views.edit_profile, name='edit_profile'),
+    # path('<str:display_name>/dashboard/', views.dashboard, name='dashboard'),
+    # path('<str:display_name>/edit-profile', views.edit_profile, name='edit_profile'),
     path('logout/', views.logout, name='logout'),
     path('loggingout/', views.loggingout, name='loggingout'),
 ]
