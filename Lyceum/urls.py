006007db395b26f12b7c59bb2e0cc9fb8d31dadb
Aiden <skrillfalcoblast@icloud.com>
2023-11-26 11:33:03 +0000

    Removing import PostSitemap

--- Lyceum/urls.py
+++ Lyceum/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from .sitemaps import PostSitemap
+# from .sitemaps import PostSitemap
 
 sitemaps = {
     # "posts": PostSitemap,
